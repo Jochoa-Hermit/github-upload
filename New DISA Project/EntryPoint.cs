e02db8ae2b8130a499c547c1e855b65218768059
Javier Ochoa <jochoa_disa@hotmail.com>
2021-03-24 13:51:54 -0700

    Added a second write line

--- New DISA Project/EntryPoint.cs
+++ New DISA Project/EntryPoint.cs
@@ -11,6 +11,7 @@ namespace New_DISA_Project
         static void Main(string[] args)
         {
             Console.WriteLine("Making a Change");
+            Console.WriteLine("Commit");
         }
     }
 }
