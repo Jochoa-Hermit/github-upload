7e2e3aed3859df478899ea2473885bf8858dfd8d
Jochoa-Hermit <jarvisochoa8a@gmail.com>
2021-04-02 08:02:45 -0600

    Created ProjectCreator Class

---
+++ New DISA Project/Consts.cs
@@ -0,0 +1,12 @@
+﻿namespace New_DISA_Project
+{
+    public static class Consts
+    {
+        public static class ProjectCreator
+        {
+            public const string MAIN_DIRECTORY = @"C:\Users\jarvi\Documents\DISA\PROYECTOS";
+            public const string CLIENT_DIRECTORY = @"MISCELLANEOUS\";
+            public const bool CONTAINS_CONTROL = true;
+        }
+    }
+}
