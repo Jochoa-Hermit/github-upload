7ff57bc5e310d5104ec3452275ed1db8be9ab2c6
Javier Ochoa <jochoa_disa@hotmail.com>
2021-05-24 11:17:14 -0600

    Moved menus to a static class, added a yes/no menu

--- New DISA Project/EntryPoint.cs
+++ New DISA Project/EntryPoint.cs
@@ -36,51 +36,24 @@ namespace New_DISA_Project
 
             newClientProject.ControlSelector(newProject.ProjectName, newProject.SystemDirectory, newProject.ControlDirectory);
 
-            //Console.WriteLine(Directory.GetCurrentDirectory());
 
-            //newProject.CreatesDirectories
-            //Console.WriteLine(newProject.MainDirectory);
+            // ********************************************* This code was used to test out the menu static method *********************************************// 
 
             // Using a dictionary to pass to the menu class
+
             var methodOptions = new Dictionary<int, Action>()
             {
                 {0, () => newClientProject.AddIOSheet((newProject.ProjectDocDirectory + $@"\F-DIS-003 Formato IO'S {newProject.ProjectName}.xlsx")) },
                 {1, () => newClientProject.ControlSelector(newProject.ProjectName, newProject.SystemDirectory, newProject.ControlDirectory) }
             };
 
-            // Testing out a static method
+            //// Testing out a static method
             string[] myStringArray = new string[] { "Jabil Project", "Xylem Project", "Misc. Project" };
-            Console.WriteLine(Menu("Select from the options", myStringArray, methodOptions));
-            ///////////////////////////////////////////////////////////////
+            Menus.MenuWithMethods("Select from the options", myStringArray, methodOptions);
+
+            // ********************************************* This code was used to test out the menu static method 
 
             Console.ReadLine();
-        }
-
-        public static int Menu(string promptString, string[] Options, Dictionary<int, Action> methodDictionary)
-        {
-            int OptionSelected = 0; // Will return this value once the selection has been made. 
-            // This code will only return the value, do whatever is needed outside the method 
-
-            Console.Write($"{promptString}:\n");
-            for (int i = 0; i < Options.Length; i++)
-            {
-                Console.Write((i + 1));
-                Console.Write(".-");
-                Console.Write(Options[i]);
-                Console.Write("\n");
-            }
-            OptionSelected = Int32.Parse(Console.ReadLine());
-
-            // We have to test this out, to determine if this will call the method with the argument passed
-            for (int i = 0; i < Options.Length; i++)
-            {
-                if ((OptionSelected - 1) == i)
-                {
-                    methodDictionary[i].Invoke();
-                }
-            }
-
-            return OptionSelected;
         }
     }
 }
