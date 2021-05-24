68220bba2705586f4608e424d1ae786f14db310b
Jochoa-Hermit <jarvisochoa8a@gmail.com>
2021-05-24 07:31:16 -0500

    Added a Menu static method, this method asks the user for prompt, a string array of options, and a dictionary of methods. We still have to test out the method implementation

--- New DISA Project/EntryPoint.cs
+++ New DISA Project/EntryPoint.cs
@@ -14,6 +14,9 @@ namespace New_DISA_Project
     {
         static void Main(string[] args)
         {
+            
+
+
             //*********Application Variables*********//
 
             ProjectCreator newProject = new ProjectCreator();
@@ -25,7 +28,7 @@ namespace New_DISA_Project
 
             newProject.SetProjectName();
             //Console.WriteLine(newProject.ProjectName);
-            
+
 
             newProject.CreatesDirectories();
 
@@ -38,7 +41,46 @@ namespace New_DISA_Project
             //newProject.CreatesDirectories
             //Console.WriteLine(newProject.MainDirectory);
 
+            // Using a dictionary to pass to the menu class
+            var methodOptions = new Dictionary<int, Action>()
+            {
+                {0, () => newClientProject.AddIOSheet((newProject.ProjectDocDirectory + $@"\F-DIS-003 Formato IO'S {newProject.ProjectName}.xlsx")) },
+                {1, () => newClientProject.ControlSelector(newProject.ProjectName, newProject.SystemDirectory, newProject.ControlDirectory) }
+            };
+
+            // Testing out a static method
+            string[] myStringArray = new string[] { "Jabil Project", "Xylem Project", "Misc. Project" };
+            Console.WriteLine(Menu("Select from the options", myStringArray, methodOptions));
+            ///////////////////////////////////////////////////////////////
+
             Console.ReadLine();
+        }
+
+        public static int Menu(string promptString, string[] Options, Dictionary<int, Action> methodDictionary)
+        {
+            int OptionSelected = 0; // Will return this value once the selection has been made. 
+            // This code will only return the value, do whatever is needed outside the method 
+
+            Console.Write($"{promptString}:\n");
+            for (int i = 0; i < Options.Length; i++)
+            {
+                Console.Write((i + 1));
+                Console.Write(".-");
+                Console.Write(Options[i]);
+                Console.Write("\n");
+            }
+            OptionSelected = Int32.Parse(Console.ReadLine());
+
+            // We have to test this out, to determine if this will call the method with the argument passed
+            for (int i = 0; i < Options.Length; i++)
+            {
+                if ((OptionSelected - 1) == i)
+                {
+                    methodDictionary[i].Invoke();
+                }
+            }
+
+            return OptionSelected;
         }
     }
 }
